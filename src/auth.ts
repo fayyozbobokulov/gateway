b95c0b1c78424db1c9355f828c95b9f0441cf1e9
fayyozbobokulov <b.fayyoz2000@gmail.com>
2023-04-07 18:08:19 +0500

    Added gateway

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, Express } from 'express';
 import jwt from 'jsonwebtoken';
 import { config } from './config';
 
@@ -13,9 +13,17 @@ export function authMiddleware(
 
 	try {
 		const decoded = jwt.verify(token, config.jwtSecret);
-		(req as any).user = decoded;
+		(req as any).session = decoded;
 		next();
 	} catch (ex) {
 		res.status(400).send('Invalid token.');
 	}
+}
+
+export const setupAuth = (app: Express, routes: any[]) => {
+	routes.forEach(r => {
+		if (r.auth) {
+			app.use(r.url, authMiddleware)
+		}
+	})
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { setupLogging } from './logging';
 import { setupProxies } from './proxy';
 import { ROUTES } from './routes';
+import { setupAuth } from './auth';
 
 const app = express();
 
 const port = process.env.PORT || 3000;
+
 setupLogging(app);
+setupAuth(app, ROUTES);
 setupProxies(app, ROUTES);
-app.get('/hello', async (req: Request, res: Response) => {
-	return res.send('HELLO');
-});
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
